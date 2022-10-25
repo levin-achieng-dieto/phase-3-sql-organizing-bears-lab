79c16d932ab983f74860afcd637a28ddfe19d9cf
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-10-25 18:28:34 +0300

    commit tree 6134791276cfffe7b8c55a7f4b25d942c2593fbd

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "F", "chocolate", "Sanguine", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 6, "F", "brown", "Melancholic", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 10, "F", "chocolate", "Phlegmatic", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Surgent Brown", 5, "F", "orange", "Choleric", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 11, "M", "pink", "Sanguine", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "M", "chocolate", "Sanguine", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, "F", "red", "Choleric", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, "M", "yellow", "Phlegmatic", true);
